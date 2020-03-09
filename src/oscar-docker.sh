5f85e9ab16c38a4788501d0862ae9eb69d06d71a
Daniel Bahrdt <daniel.git@funroll-loops.de>
2020-03-09 16:21:00 +0100

    Change layout to improve support for doker-compose configs to change config options without rebuilding the image

--- src/oscar-docker.sh
+++ src/oscar-docker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+#	-e 'OSM_SOURCE_REMOTE_URL=http://download.geofabrik.de/europe/andorra-latest.osm.pbf' \
+#	-e 'OSCAR_SOURCE_REMOTE_URL=file:///archive/oscar/20200307.tar.bz2' \
 docker run \
 	-v oscar-source:/source \
 	-v oscar-scratch-fast:/scratch/fast \
@@ -6,10 +9,10 @@ docker run \
 	-v oscar-next:/next \
 	-v oscar-active:/active \
 	-v oscar-archive:/archive \
-	-e 'SOURCE_REMOTE_URL=http://download.geofabrik.de/europe/andorra-latest.osm.pbf' \
 	-e 'UPDATES=enabled' \
 	-e 'ARCHIVE=enabled' \
 	-e 'CLEAN_ARCHIVE=enabled' \
+	-e 'OSCAR_SOURCE_REMOTE_URL=file:///archive/oscar/20200306.tar.bz2' \
 	-p "14080:80" \
 	--rm \
 	oscar \
